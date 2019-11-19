e389d165ef88aea15853439436e50899cf442abb
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-19 17:35:49 -0500

    up to v0.6.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,33 +1,36 @@
 #!/usr/bin/env sh
-VER=${1:-v0.5.0}
 DIR=~/Downloads
-MIRROR=https://github.com/rancher/rio/releases/download/$VER
+MIRROR=https://github.com/rancher/rio/releases/download
 
 dl()
 {
-    local os=$1
-    local arch=$2
+    local ver=$1
+    local os=$2
+    local arch=$3
 
     local checksums=sha256sum-$arch.txt
-    local lchecksums=$DIR/rio-sha256sum-$arch-$VER.txt
+    local lchecksums=$DIR/rio-sha256sum-$arch-$ver.txt
 
     if [ ! -e $lchecksums ]
     then
-        wget -q -O $lchecksums $MIRROR/$checksums
+        wget -q -O $lchecksums $MIRROR/$ver/$checksums
     fi
 
     local file=rio-$os-$arch
-    local url=$MIRROR/$file
+    local url=$MIRROR/$ver/$file
 
     printf "    # %s\n" $url
     printf "    %s-%s: sha256:%s\n" $os $arch `fgrep $file $lchecksums | awk '{print $1}'`
 }
 
-printf "  %s:\n" $VER
-dl darwin amd64
-dl linux amd64
-dl linux arm
-dl linux arm64
-dl windows amd64
+dl_ver() {
+    local ver=$1
+    printf "  %s:\n" $ver
+    dl $ver darwin amd64
+    dl $ver linux amd64
+    dl $ver linux arm
+    dl $ver linux arm64
+    dl $ver windows amd64
+}
 
-
+dl_ver ${1:-v0.6.0}
